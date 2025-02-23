07b7f0829547667b457d8c31c40df43e11582297
Leonardo Pesce <196577953+PesceLeonardo@users.noreply.github.com>
2025-02-23 17:52:31 +0100

    Build main logic structure

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ import {add, subtract, multiply, isZero, divide, integerDivide, modulusDivide, c
 // DOM Elements
 
 const digitsNodeList = document.querySelectorAll(".number");
+const operatorsNodeList = document.querySelectorAll(".operator");
 
 const plusOperator = document.querySelector(".addition");
 const minusOperator = document.querySelector(".subtraction");
@@ -25,3 +26,59 @@ const equalOperator = document.querySelector(".equal");
 
 const calculatorDisplay = document.querySelector(".display");
 
+
+
+// Global Variables
+
+const elements = {
+  firstOperand: "",
+  operationFunction: null,
+  secondOperand: "",
+};
+
+let display = "";
+
+// Enter Digits
+
+digitsNodeList.forEach(digitButton => digitButton.addEventListener("click", function(e) {
+  const digit = e.target.textContent;
+  calculatorDisplay.textContent.length < 13 ? calculatorDisplay.textContent += digit : null;
+}));
+
+operatorsNodeList.forEach(operatorButton => operatorButton.addEventListener("click", function(e) {
+  elements.firstOperand = calculatorDisplay.textContent;
+  calculatorDisplay.textContent = 0;
+
+  switch (e.target.textContent) {
+    case "+":
+      elements.operationFunction = add;
+      break;
+
+    case "−":
+      elements.operationFunction = subtract;
+      break;
+
+    case "×":
+      elements.operationFunction = multiply;
+      break;
+
+    case "÷":
+      elements.operationFunction = divide;
+      break;
+
+    case "%":
+      elements.operationFunction = modulusDivide;
+      break;
+
+    case "//":
+      elements.operationFunction = integerDivide;
+      break;
+
+  }
+}));
+
+equalOperator.addEventListener("click", function() {
+  elements.secondOperand = calculatorDisplay.textContent;
+  const result = elements.operationFunction(elements.firstOperand, elements.secondOperand);
+  calculatorDisplay.textContent = result;
+});
